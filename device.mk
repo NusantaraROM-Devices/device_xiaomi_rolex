01e432a68eddb013211bf3c8e462fca29fb1877c
GiaSen <giammasena@gmail.com>
2020-09-11 16:16:35 +0200

    rolex: Copy VNDK-29 libbinderthreadstate.so to vendor
    Signed-off-by: Ahmad Rulim <mfalkia342@gmail.com>

--- device.mk
+++ device.mk
@@ -445,6 +445,7 @@ PRODUCT_PACKAGES += \
 
 #Vndk prebuilts
 PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-sp/libbinderthreadstate.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libbinderthreadstate.so \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full.so \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-lite.so
 
