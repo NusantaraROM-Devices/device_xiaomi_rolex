5e2d5d6afa0b8bfb974e8e51b1ccdd885fb5bba8
Nick <nikhilnick00007@gmail.com>
2020-09-15 12:50:13 +0530

    rolex: copy libprotobuf-cpp-full/lite prebuilts from vndk29

--- device.mk
+++ device.mk
@@ -421,6 +421,11 @@ PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
     android.hardware.vibrator@1.0-service
 
+#Vndk prebuilts
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full.so \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-lite.so
+
 # WiFi
 PRODUCT_PACKAGES += \
     libwpa_client \
