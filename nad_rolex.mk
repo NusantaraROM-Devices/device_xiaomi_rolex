98dd590779f325dfcf187b71132c471a0876503b
dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>
2020-09-15 20:27:51 +0000

    rolex: Define target boot animation resolution

--- nad_rolex.mk
+++ nad_rolex.mk
@@ -27,6 +27,8 @@ $(call inherit-product, packages/apps/NusantaraParts/nadproject.mk)
 # Inherit from rolex device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
+TARGET_BOOT_ANIMATION_RES := 720
+
 # Nusantara Properties
 NAD_BUILD_TYPE := OFFICIAL
 
