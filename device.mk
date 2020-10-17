adc77d9a22b9118486cbf04d7c05604f96902f9b
GiaSen <giammasena@gmail.com>
2020-10-17 19:54:52 +0700

    rolex: Build libhidltransport
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -99,7 +99,9 @@ PRODUCT_COPY_FILES += \
 # Binder
 PRODUCT_PACKAGES += \
     libhwbinder \
-    libhwbinder.vendor
+    libhwbinder.vendor \
+    libhidltransport \
+    libhidltransport.vendor
 
 # Camera
 PRODUCT_PACKAGES += \
